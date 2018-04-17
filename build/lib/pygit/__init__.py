bba1e744ea35bad2f9c22ca04cf3dbec5311ccd6
orji chidi <orjichidi95@gmail.com>
2018-04-17 06:53:34 +0000

    sanitation

--- build/lib/pygit/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Docstring"""
-
-from .main import (
-    cleanup, check_git_support, is_git_repo, need_attention, initialize,
-    Commands, show_repos, load, load_multiple, pull, push, all_status
-)
-
-__all__ = ["all_status", "cleanup", "check_git_support", "is_git_repo", "need_attention", "initialize",
-           "Commands", "show_repos", "load", "load_multiple", "pull", "push", "all_status"]
