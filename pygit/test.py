ba1af1abf5466eff794c172347a24f544b292705
Chidi Orji <orjichidi95@gmail.com>
2019-01-08 19:23:50 +0100

    chore: minor change

--- pygit/test.py
+++ pygit/test.py
@@ -17,5 +17,5 @@ from send2trash import send2trash
 # from .main import (
 #     USERHOME, DESKTOP, STATUS_DIR, BASE_DIR, SHELF_DIR, TEST_DIR, TIME_STAMP,
 #     cleanup, check_git_support, is_git_repo, need_attention, initialize,
-#     Commands, show_repos, load, load_multiple, pull, push, all_status
+#     Commands, repos, load, load_multiple, pull, push, all_status
 # )
