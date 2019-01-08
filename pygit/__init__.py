b1bc68471fc93feda747136a21be51a878a29fd9
Chidi Orji <orjichidi95@gmail.com>
2019-01-08 19:33:46 +0100

    chore: update imports

--- pygit/__init__.py
+++ pygit/__init__.py
@@ -1,10 +1,8 @@
-"""Docstring"""
-
 from .__version__ import __version__
 from .pygit import (
-    cleanup, check_git_support, is_git_repo, initialize,
-    Commands, show_repos, load, load_multiple, pull, push, all_status
+    cleanup, check_git_support, is_git_repo, initialize, update,
+    Commands, repos, load, load_multiple, pull, push, all_status
 )
 
-__all__ = ["all_status", "cleanup", "check_git_support", "is_git_repo", "initialize",
-           "Commands", "show_repos", "load", "load_multiple", "pull", "push"]
+__all__ = ['all_status', 'cleanup', 'check_git_support', 'is_git_repo', 'initialize', 'update',
+           'Commands', 'repos', 'load', 'load_multiple', 'pull', 'push']
